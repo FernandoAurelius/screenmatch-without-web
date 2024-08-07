ab35eb68f29df12f1a4ce10f4e0394df2da9689e
Miguel Fernando Flores <miguelfernandoaurelius@gmail.com>
2024-08-06 21:34:36 -0300

    feat(User Interface): Implement functionality for displaying episodes of a series from a specific year, add getRating, getReleaseDate, getSeason, getEpisodeNumber and toString methods in EpisodeModel class.

---
+++ src/main/java/br/com/floresdev/screenmatch/models/EpisodeModel.java
@@ -0,0 +1,64 @@
+package br.com.floresdev.screenmatch.models;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class EpisodeModel {
+
+    private Number season;
+
+    private String title;
+
+    private Integer episodeNumber;
+
+    private Double rating;
+
+    private LocalDate releaseDate;
+
+    public EpisodeModel(Integer episodeNumber, EpisodeDataModel episode) {
+        this.season = episodeNumber;
+        this.title = episode.title();
+        this.episodeNumber = episode.number();
+        try {
+            this.rating = Double.valueOf(episode.rating());
+        } catch (NumberFormatException e) {
+            this.rating = 0.0;
+        }
+        try {
+            this.releaseDate = LocalDate.parse(episode.releaseDate());
+        } catch (DateTimeParseException e) {
+            this.releaseDate = null;
+        }
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public Number getSeason() {
+        return season;
+    }
+
+    public Integer getEpisodeNumber() {
+        return episodeNumber;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("""
+                Episode: "%s",
+                Season: %s,
+                Number: %d,
+                Release date: %s,
+                Rating: %s.
+                """, title, season, episodeNumber, releaseDate, rating);
+    }
+}
