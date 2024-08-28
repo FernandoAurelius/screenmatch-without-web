a6efac0de140b274782c1f743bc4d4aac6314c2c
Miguel Fernando Flores Barreto <miguel.barreto@arqdigital.com.br>
2024-08-28 17:59:30 -0300

    feat(model): add actors and poster address to SeriesDataModel
    Adds actors and poster address fields to the SeriesDataModel to provide more comprehensive information about series.

--- src/main/java/br/com/floresdev/screenmatch/model/SeriesDataModel.java
+++ src/main/java/br/com/floresdev/screenmatch/model/SeriesDataModel.java
@@ -7,7 +7,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public record SeriesDataModel(@JsonAlias("Title") String title, @JsonAlias("Year") String yearsInActivity,
                               @JsonAlias("Runtime") String runtime, @JsonAlias("Genre") String genre,
                               @JsonAlias("Language") String language, @JsonAlias("Plot") String plot,
-                              @JsonAlias("imdbRating") String rating, @JsonAlias("totalSeasons") int seasons) {
+                              @JsonAlias("imdbRating") String rating, @JsonAlias("totalSeasons") int seasons,
+                              @JsonAlias("Actors") String actors, @JsonAlias("Poster") String posterAddress) {
 
     @Override
     public String toString() {
