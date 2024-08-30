a7ba71016549bbd30cadb04ae6173cb47257af58
Miguel Fernando Flores <miguelfernandoaurelius@gmail.com>
2024-08-29 21:41:33 -0300

    feat(application): add translation feature to series plot
    This commit adds a new feature to the application that allows users to translate the plot of a series to a different language.
    
    The following changes were made:
    
    - Added a new method `getTranslationLanguage()` to the `UserInteraction` class to prompt the user for the desired translation language.
    - Added a new case to the `UserInterface` class to handle the translation feature.
    - Modified the `SeriesModel` class to accept a language parameter in the constructor and use the `TranslationService` to translate the plot.
    - Added a new method `showSeries()` to the `DisplayService` class to display the translated series information.
    - Modified the `TranslationService` class to make the `getTranslation()` method public.

--- src/main/java/br/com/floresdev/screenmatch/model/SeriesModel.java
+++ src/main/java/br/com/floresdev/screenmatch/model/SeriesModel.java
@@ -1,5 +1,8 @@
 package br.com.floresdev.screenmatch.model;
 
+import br.com.floresdev.screenmatch.service.translate.TranslationService;
+
+import java.io.UnsupportedEncodingException;
 import java.util.OptionalDouble;
 
 public class SeriesModel {
@@ -9,23 +12,80 @@ public class SeriesModel {
     private final String runtime;
     private final Category genre;
     private final String language;
-    private final String plot;
+    private String plot;
     private final Double imdbRating;
     private final Integer totalSeasons;
     private final String actors;
     private final String posterAddress;
 
-    public SeriesModel(SeriesDataModel series) {
-        this.title = series.title();
-        this.yearsInActivity = series.yearsInActivity();
-        this.runtime = series.runtime();
-        this.genre = Category.fromString(series.genre().split(",")[0].trim());
-        this.language = series.language();
-        this.plot = series.plot();
-        this.imdbRating = OptionalDouble.of(Double.parseDouble(series.rating())).orElse(0);
-        this.totalSeasons = series.seasons();
-        this.actors = series.actors();
-        this.posterAddress = series.posterAddress();
+    public SeriesModel(SeriesDataModel seriesData) {
+        this.title = seriesData.title();
+        this.yearsInActivity = seriesData.yearsInActivity();
+        this.runtime = seriesData.runtime();
+        this.genre = Category.fromString(seriesData.genre().split(",")[0].trim());
+        this.language = seriesData.language();
+        this.plot = seriesData.plot();
+        this.imdbRating = OptionalDouble.of(Double.parseDouble(seriesData.rating())).orElse(0);
+        this.totalSeasons = seriesData.seasons();
+        this.actors = seriesData.actors();
+        this.posterAddress = seriesData.posterAddress();
+    }
+
+    public SeriesModel(SeriesModel series, String language) {
+        this.title = series.getTitle();
+        this.yearsInActivity = series.getYearsInActivity();
+        this.runtime = series.getRuntime();
+        this.genre = series.getGenre();
+        this.language = series.getLanguage();
+        try {
+            this.plot = TranslationService.getTranslation(series.getPlot(), language);
+        } catch (UnsupportedEncodingException e) {
+            this.plot = series.getPlot();
+        }
+        this.imdbRating = series.getImdbRating();
+        this.totalSeasons = series.getTotalSeasons();
+        this.actors = series.getActors();
+        this.posterAddress = series.getPosterAddress();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getYearsInActivity() {
+        return yearsInActivity;
+    }
+
+    public String getRuntime() {
+        return runtime;
+    }
+
+    public Category getGenre() {
+        return genre;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public Double getImdbRating() {
+        return imdbRating;
+    }
+
+    public Integer getTotalSeasons() {
+        return totalSeasons;
+    }
+
+    public String getActors() {
+        return actors;
+    }
+
+    public String getPosterAddress() {
+        return posterAddress;
     }
 
     @Override
@@ -43,9 +103,5 @@ public class SeriesModel {
                 Poster: (%s)
                 """, title, yearsInActivity, runtime, genre, language, plot, imdbRating, totalSeasons, actors,
                 posterAddress);
-    }
-
-    public int getTotalSeasons() {
-        return totalSeasons;
     }
 }
